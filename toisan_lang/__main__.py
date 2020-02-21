0079d0a618f989dcfd08a5311eca215ba47a2699
Zeheng Li <imzehengl@gmail.com>
2020-02-21 11:37:47 -0700

    update main entry point

--- toisan_lang/__main__.py
+++ toisan_lang/__main__.py
@@ -9,9 +9,13 @@ parser = argparse.ArgumentParser(
         ", aka Taishanese, a dialect of Cantonese"
     )
 )
-parser.add_argument(
-    "program", type=str, nargs=1, help="the program written in toisan-lang"
-)
+parser.add_argument("program", type=str, nargs=1, help="program written in toisan-lang")
+parser.add_argument("--tree", action="store_true", help="show the parse tree")
 args = parser.parse_args()
 
-print(ToisanGrammar.parse(args.program))
+
+if args.tree:
+    pare_tree = ToisanGrammar.parse(args.program)
+    print(ToisanGrammar.repr_parse_tree(pare_tree))
+else:
+    raise NotImplementedError("tree_factory not implemented")
