eb1a807576b04e76bc776891700886948c90f907
Zeheng Li <imzehengl@gmail.com>
2020-02-28 00:02:43 -0700

    forgot the test

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -3,9 +3,12 @@ import pytest
 from toisan_lang import ToisanCompiler, ToisanGrammar
 
 from .testcases.compiler.assign_statement import data as data_assign_statement
+from .testcases.compiler.if_statement import data as data_if_statement
+
+data = data_assign_statement + data_if_statement
 
 
-@pytest.mark.parametrize("program, transpiled", data_assign_statement)
+@pytest.mark.parametrize("program, transpiled", data)
 def test_assign_statement(program, transpiled):
     toisan_compiler = ToisanCompiler()
     tree_factory = toisan_compiler.tree_factory
